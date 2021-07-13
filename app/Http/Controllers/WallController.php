6dc030d8a2c4a41afb3ef5041576e0bbdf0a2e5d
Abhishek bali <abhishekbali15oct@gmail.com>
2021-07-13 10:01:15 +0530

    delete

--- app/Http/Controllers/WallController.php
+++ app/Http/Controllers/WallController.php
@@ -92,7 +92,7 @@ class WallController extends Controller
 
     public function delete($id)
     {
-        $wallName=Wall::where('wallId',$id)->first();
+        $wallName=Wall::where('wallId',$id)->first()->get();
         
 
         $userDetails=userDetail::all();
